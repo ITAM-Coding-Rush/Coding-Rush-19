31643ed024a74464db74598feee20ceeac5c6739
YOSSHUA <yocisneros@outlook.com>
2021-04-08 23:23:59 -0500

    ahora si el bueno

--- semana1/bayern-for-the-win/solutions/solution.py
+++ semana1/bayern-for-the-win/solutions/solution.py
@@ -3,14 +3,14 @@ R1 = int(input())
 R2 = int(input())
 P2 = int(input())
 
-GP = P1 + P2
+GB = P1 + P2
 GR = R1 + R2
 
-if GP > GR:
-  print("Gana Porto")
+if GB > GR:
+  print("Gana Bayern")
   
-if GP < GR:
-  print("Pierde Porto")
+if GB < GR:
+  print("Pierde Bayern")
   
-if GP == GR:
+if GB == GR:
   print("ChinChanPu")
