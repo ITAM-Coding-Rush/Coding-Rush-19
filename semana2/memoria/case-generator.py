f3d3f420d208c436985a6f9117698dd9d1b9544c
CaDe27 <cdelgadoelizondo@gmail.com>
2021-04-16 01:08:28 -0500

    arreglo del case generator de memoria

--- semana2/memoria/case-generator.py
+++ semana2/memoria/case-generator.py
@@ -1,9 +1,10 @@
 import random
 import os
-
+import string 
 baseString = """
 {N}
 {A}
+{M}
 {B}
 """.strip(' \t\n\r')
 
@@ -13,12 +14,13 @@ for i in range(casos):
     MAXN = 100000 // ((casos - i))
     n = random.randint(1, MAXN)
     p = random.randint(1, MAXN)
-    A = [ ''.join(random.choice(letters) for i in range(10)) for j in range (n)]
+    A = [ ''.join(random.choice(string.ascii_lowercase) for i in range(10)) for j in range (n)]
     B = [random.randint(0, n-1) for i in range(p)]
 
     case = {
         'N': str(n),
         'A': "\n".join(str(x) for x in A),
+        'M': str(p),
         'B': "\n".join(str(x) for x in B)
     }
     caseName = "{}.in".format(i)
