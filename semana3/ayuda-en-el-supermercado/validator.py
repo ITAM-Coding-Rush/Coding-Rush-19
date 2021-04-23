982e7116a6cb0ca9d5e0cba602e4c451c94c0c33
CaDe27 <cdelgadoelizondo@gmail.com>
2021-04-23 18:05:07 -0500

    Actualización validador

--- semana3/ayuda-en-el-supermercado/validator.py
+++ semana3/ayuda-en-el-supermercado/validator.py
@@ -4,9 +4,14 @@ with open('data.out', 'r') as handle:
     res_oficial = handle.read()
 
 lines_oficial = res_oficial.split('\n')
+while len(lines_oficial) > 0 and lines_oficial[-1] == '':
+    lines_oficial.pop()
 
 res_contestant = sys.stdin.read()
+
 lines_contestant = res_contestant.split('\n')
+while len(lines_contestant) > 0 and lines_contestant[-1] == '':
+    lines_contestant.pop()
 
 lines_contestant.sort()
 lines_oficial.sort()
